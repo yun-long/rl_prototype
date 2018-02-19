fix policy initialization with too large variance problem

rl/env/random_jump.py
@@ -38,7 +38,7 @@ class RandomJumpEnv(gym.Env):
         return np.array(self.state)
 
     def _step(self, action):
-        noise = np.random.uniform(low=-0.01,high=0.01)
+        noise = np.random.uniform(low=-0.1,high=0.1)
         position = self.state[0]
         #
         force = min(max(action[0], self.min_action), self.max_action)
@@ -48,10 +48,10 @@ class RandomJumpEnv(gym.Env):
         done = bool(position >= (self.goal_position-0.05) and position <= (self.goal_position+0.05))
         #
         # cost = np.square(self.goal_position - position)
-        cost = np.square(self.goal_position - position) + np.square(action[0])
+        cost = np.square(self.goal_position - position) + math.pow(action[0],2)*0.1
         reward = -cost
         if done:
-            reward = 10
+            reward = 1
         # reward = np.atleast_1d(reward)
         self.state = [min(max(position, self.min_position), self.max_position)]
         #

rl/misc/plot_rewards.py
@@ -26,7 +26,7 @@ def plot_tr_ep_rs(tr_ep_rs, show=True):
         plt.show()
     return fig
 
-def plot_coeff_tr_ep_rs(mean_rewards, coeff, label=r'$\alpha$ = ',show=True):
+def plot_coeff_tr_ep_rs(mean_rewards, coeff, label=r'$\alpha$ = ', logR = False, show=True):
     fig = plt.figure()
     # plt.hold('on')
     ax = fig.add_subplot(111)
@@ -35,8 +35,10 @@ def plot_coeff_tr_ep_rs(mean_rewards, coeff, label=r'$\alpha$ = ',show=True):
     # c = ['b', 'm', 'r']
 
     for l in range(len(coeff)):
-        # logRew = -np.log(-mean_rewards)
-        logRew = mean_rewards
+        if logR:
+            logRew = -np.log(-mean_rewards)
+        else:
+            logRew = mean_rewards
         r_mean = np.mean(logRew[:,:,l],axis=1)
         r_std = np.std(logRew[:,:,l],axis=1)
         plt.fill_between(range(mean_rewards.shape[0]), r_mean - r_std, r_mean + r_std, alpha=0.3)

rl/policy/gp_linear_mean.py
@@ -13,7 +13,7 @@ class GPLinearMean(GaussianPolicy):
         self.featurizer = featurizer
         #
         self.Mu_theta = np.random.randn(self.num_features, self.num_actions) / np.sqrt(self.num_features)
-        self.Sigma_action = np.eye(self.num_actions) * 1e4 # for exploration in parameter space
+        self.Sigma_action = np.eye(self.num_actions) * 1e1 # for exploration in parameter space
         super().__init__()
 
     def predict_action(self, state):
@@ -64,4 +64,5 @@ class GPLinearMean(GaussianPolicy):
                 if done:
                     time.sleep(1)
                     break
+        self.env.render(close=True)
 