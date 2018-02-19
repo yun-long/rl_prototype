b414b632680ec1435c744ecba9d1b5c7f4c78da9
Yunlong Song <yun-long.song@outlook.com>
2018-02-19 18:09:11 +0100

    trying TRPO not working

--- rl/policy/gp_fc.py
+++ rl/policy/gp_fc.py
@@ -5,6 +5,7 @@ Reference: John Schulman, Trust Region Policy Optimization
 """
 
 import tensorflow as tf
+import numpy as np
 
 class GPFC(object):
     """
@@ -14,55 +15,64 @@ class GPFC(object):
         self.n_action = env.action_space.shape[0]
         self.n_state = env.observation_space.shape[0]
         self.learning_rate = learning_rate
+        self.model()
 
     def model(self):
-        # define input layer, input states
-        self.state = tf.placeholder(tf.float32, shape=[None, self.n_state], name="states")
-        # define hidden layers
-        with tf.variable_scope('l1'):
-            w1 = tf.get_variable(name="w1",
-                                shape=[self.n_state, 30],
-                                dtype=tf.float32,
-                                initializer=tf.random_normal_initializer())
-            b1 = tf.get_variable(name="b1",
-                                shape=[30],
-                                dtype=tf.float32,
-                                initializer=tf.constant_initializer())
-            h0 = tf.nn.relu(tf.matmul(self.state, w1) + b1)
-        with tf.variable_scope('l2'):
-            w2 = tf.get_variable(name="w2",
-                                 shape=[30, self.n_action],
-                                 dtype=tf.float32,
-                                 initializer=tf.random_normal_initializer())
-            b2 = tf.get_variable(name="b2",
-                                 shape=[self.n_action],
-                                 dtype=tf.float32,
-                                 initializer=tf.constant_initializer())
-            self.mu = tf.matmul(h0, w2) + b2 # output of the neural nets, namely, the predicted actions
-            self.mu = tf.squeeze(self.mu)
-        # the true actions.
-        self.sigma = tf.constant(1e1, dtype=tf.float32, shape=[self.n_action])
-        self.norm_dist = tf.contrib.distributions.Normal(self.mu, self.sigma)
-        self.action = self.norm_dist._sample_n(1)
-        #
-        self.target_adv = tf.placeholder(tf.float32, shape=[None, 1], name="advantages")
-        #
-        with tf.variable_scope('loss'):
-            self.loss = -self.norm_dist.log_prob(self.action) * self.target_adv
+        with tf.variable_scope('policy'):
+            # define input layer, input states
+            self.state = tf.placeholder(tf.float32, shape=[None, self.n_state], name="states")
+            # define hidden layers
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable(name="w1",
+                                     shape=[self.n_state, 30],
+                                     dtype=tf.float32,
+                                     initializer=tf.random_normal_initializer())
+                b1 = tf.get_variable(name="b1",
+                                     shape=[30],
+                                     dtype=tf.float32,
+                                     initializer=tf.constant_initializer())
+                h0 = tf.nn.relu(tf.matmul(self.state, w1) + b1)
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable(name="w2",
+                                     shape=[30, self.n_action],
+                                     dtype=tf.float32,
+                                     initializer=tf.random_normal_initializer())
+                b2 = tf.get_variable(name="b2",
+                                     shape=[self.n_action],
+                                     dtype=tf.float32,
+                                     initializer=tf.constant_initializer())
+                self.mu = tf.matmul(h0, w2) + b2 # output of the neural nets, namely, the predicted actions
+                self.mu = tf.squeeze(self.mu)
+            # the true actions.
+            self.sigma = tf.constant(1e1, dtype=tf.float32, shape=[self.n_action])
+            self.norm_dist = tf.contrib.distributions.Normal(self.mu, self.sigma)
 
-        with tf.variable_scope('optimizer'):
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-            self.train_op = self.optimizer.minimize(loss=self.loss,
-                                                    global_step=tf.contrib.framework.get_global_step())
+            self.pred_action = self.norm_dist.sample([1])
+            #
+            # TODO:
+            self.target_action = tf.placeholder(tf.float32, shape=[None, self.n_action], name="actions")
+            self.target_adv = tf.placeholder(tf.float32, shape=[None, 1], name="advantages")
+            #
+            with tf.variable_scope('loss'):
+                # TODO:
+                self.loss = -self.norm_dist.log_prob(self.target_action) * self.target_adv
+
+            with tf.variable_scope('optimizer'):
+                self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                self.train_op = self.optimizer.minimize(loss=self.loss,
+                                                        global_step=tf.contrib.framework.get_global_step())
 
     def predict(self, state, sess=None):
+        state = np.reshape(state, (-1, state.shape[0]))
         sess = sess or tf.get_default_session()
-        return sess.run([self.action, {self.state: state}])
+        action = sess.run([self.pred_action], {self.state: state})[0]
+        return action[0]
 
     def train(self, state, action, adv, sess=None):
+        # TODO: not working
         sess = sess or tf.get_default_session()
         feed_dict = {self.state: state,
-                     self.action: action,
+                     self.target_action: action,
                      self.target_adv: adv}
         _, loss = sess.run([self.train_op, self.loss], feed_dict)
         return loss
--- rl/value/value_fc.py
+++ rl/value/value_fc.py
@@ -1,49 +1,54 @@
 import tensorflow as tf
+import numpy as np
 
 class ValueFC(object):
     """
     Value function approximator for continous states environments
     """
     def __init__(self, env, learning_rate=1e-2):
-        self.n_state = env.observation_state.shape[0]
+        self.n_state = env.observation_space.shape[0]
         self.learning_rate = learning_rate
+        self.model()
 
     def model(self):
-        self.state = tf.placeholder(tf.float32, shape=[None, self.n_state], name="states")
-        # define hidden layers
-        with tf.variable_scope('l1'):
-            w1 = tf.get_variable('w1',
-                                 shape=[self.n_state, 30],
-                                 dtype=tf.float32,
-                                 initializer=tf.random_normal_initializer())
-            b1 = tf.get_variable(name='b1',
-                                 shape=[30],
-                                 dtype=tf.float32,
-                                 initializer=tf.constant_initializer())
-            h0 = tf.nn.relu(tf.matmul(self.state, w1)+b1)
-        with tf.variable_scope('l2'):
-            w2 = tf.get_variable('w2',
-                                 shape=[30, 1],
-                                 initializer=tf.random_normal_initializer())
-            b2 = tf.get_variable('b2',
-                                 shape=[1],
-                                 initializer=tf.constant_initializer())
-            self.val = tf.matmul(h0, w2) + b2
-            self.val = tf.squeeze(self.val)
-        #
-        self.target_val = tf.placeholder(tf.float32, shape=[None, 1], name='target value')
-        with tf.variable_scope('loss'):
-            self.loss = tf.squared_difference(self.target_val, self.val)
+        with tf.variable_scope('v'):
+            self.state = tf.placeholder(tf.float32, shape=[None, self.n_state], name="states")
+            # define hidden layers
+            with tf.variable_scope('l1'):
+                w1 = tf.get_variable('w1',
+                                     shape=[self.n_state, 30],
+                                     dtype=tf.float32,
+                                     initializer=tf.random_normal_initializer())
+                b1 = tf.get_variable(name='b1',
+                                     shape=[30],
+                                     dtype=tf.float32,
+                                     initializer=tf.constant_initializer())
+                h0 = tf.nn.relu(tf.matmul(self.state, w1)+b1)
+            with tf.variable_scope('l2'):
+                w2 = tf.get_variable('w2',
+                                     shape=[30, 1],
+                                     initializer=tf.random_normal_initializer())
+                b2 = tf.get_variable('b2',
+                                     shape=[1],
+                                     initializer=tf.constant_initializer())
+                self.val = tf.matmul(h0, w2) + b2
+                # self.val = tf.squeeze(self.val)
+            #
+            self.target_val = tf.placeholder(tf.float32, shape=[None, 1], name='advantages')
+            with tf.variable_scope('loss'):
+                self.loss = tf.squared_difference(self.target_val, self.val)
 
-        with tf.variable_scope('optimizer'):
-            self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
-            self.train_opt = self.optimizer.minimize(self.loss,
-                                                     global_step=tf.contrib.framework.get_global_step())
+            with tf.variable_scope('optimizer'):
+                self.optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+                self.train_opt = self.optimizer.minimize(self.loss,
+                                                         global_step=tf.contrib.framework.get_global_step())
 
 
     def predict(self, state, sess=None):
+        state = np.reshape(state, (-1, state.shape[0]))
         sess = sess or tf.get_default_session()
-        return sess.run([self.val], {self.state: state})
+        val = sess.run([self.val], {self.state: state})
+        return val[0]
 
     def train(self, state, val, sess=None):
         sess = sess or tf.get_default_session()
