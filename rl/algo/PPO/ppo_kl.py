b7f69027020f48cf85cf899865c2f4c1698e42d5
Yunlong Song <yun-long.song@outlook.com>
2018-02-21 18:54:15 +0100

    create sampler

--- rl/algo/PPO/ppo_kl.py
+++ rl/algo/PPO/ppo_kl.py
@@ -2,6 +2,7 @@ from rl.tf.models import MLP
 from rl.policy.gp_mlp import GaussianPolicy_MLP
 from rl.value.value_mlp import ValueMLP
 from rl.env.env_list import env_IDs
+from rl.sampler.advanced_sampler import AdvancedSampler
 #
 import gym
 import tensorflow as tf
@@ -20,41 +21,70 @@ opts = {'obj_ID': 0, # choose the surrogate objectives
         'epsilon': 0.2,
         'bathch_size': 32}
 #
-env = gym.make(env_IDs.Pendulum)
-s_dims = env.observation_space.shape[0]
-a_dims = env.action_space.shape[0]
-#
-pol_nn_layers = [s_dims, 64, 64, a_dims]
-pol_nn_activ  = [tf.nn.relu, tf.nn.relu, tf.nn.tanh]
-val_nn_layers = [s_dims, 64, 64, 1]
-val_nn_activ  = [tf.nn.relu, tf.nn.relu, tf.nn.tanh]
-#
 class PPO(object):
 
-    def __init__(self, policy, value, sess):
+    def __init__(self, policy_mlp, value_mlp, sess=None, epsilon=0.2, c_lrate=1e-4, a_lrate=1e-4):
+        self.pol_mlp = policy_mlp
+        self.val_mlp = value_mlp
+        self.sess = sess or tf.get_default_session()
+
+        # actor and critic
+        self._define_critic(c_lrate)
+        self._define_actor(beta=0.5, a_lrate=a_lrate)
+
+        #
+
+    def _define_critic(self, c_lrate):
+        self.target_v = tf.placeholder(tf.float32, shape=[None, 1], name='target_v')
+        self.loss_c = tf.losses.mean_squared_error(self.val_mlp.out, self.target_v)
+        self.train_opt_c = tf.train.AdamOptimizer(c_lrate).minimize(self.loss_c)
+
+    def _define_actor(self,a_lrate, beta=3.):
+        self.target_adv = tf.placeholder(tf.float32, shape=[None, 1], name='target_adv')
+        self.old_prob = tf.placeholder(tf.float32, shape=[None, 1], name='old_prob')
+        new_prob = self.pol_mlp.log_prob
+        ratio = new_prob / self.old_prob
+        kl = tf.contrib.distributions.kl(self.old_prob, new_prob)
+        self.loss_a = tf.losses.mean_squared_error(ratio * self.target_adv, beta * kl)
+        self.train_opt_a = tf.train.AdamOptimizer(a_lrate).minimize(self.loss_a)
+
+    def train_critic(self, states, target_v):
+        feed_dict = {self.val_mlp.x: states,
+                     self.target_v: target_v}
+        return self.sess.run(self.loss_c, feed_dict)
+
+    def train_actor(self, states, actions, target_adv):
+        feed_dict = {self.pol_mlp.mlp.x: states,
+                     self.pol_mlp.test_action: actions,
+                     self.target_adv: target_adv}
+        return self.sess.run(self.loss_a, feed_dict)
+
+    def eval_critic(self):
         pass
 
-    def train_v(self):
+    def eval_actor(self):
         pass
-
-    def train_pol(self):
-        pass
-
-    def eval_v(self):
-        pass
-
-    def eval_pol(self):
 
 
 
 if __name__ == '__main__':
     seed = 123
     np.random.seed(seed)
-    env.seed(seed)
     tf.set_random_seed(seed)
     #
+    env = gym.make(env_IDs.Pendulum)
+    s_dims = env.observation_space.shape[0]
+    a_dims = env.action_space.shape[0]
+    #
+    pol_nn_layers = [s_dims, 64, 64, a_dims]
+    pol_nn_activ  = [tf.nn.relu, tf.nn.relu, tf.nn.tanh]
+    val_nn_layers = [s_dims, 64, 64, 1]
+    val_nn_activ  = [tf.nn.relu, tf.nn.relu, tf.nn.tanh]
+    env.seed(seed)
+    #
     sess = tf.Session()
-    policy_fn = GaussianPolicy_MLP(pol_nn_layers, pol_nn_activ, sess)
-    value_fn = ValueMLP(val_nn_layers, val_nn_activ, sess)
-    ppo = PPO(policy_fn, value_fn, sess)
+    pol_fn = GaussianPolicy_MLP(pol_nn_layers, pol_nn_activ, sess)
+    val_fn = ValueMLP(val_nn_layers, val_nn_activ, sess)
+    ppo = PPO(pol_fn, val_fn, sess=sess)
+    sampler = AdvancedSampler(env)
     #
--- rl/sampler/advanced_sampler.py
+++ rl/sampler/advanced_sampler.py
@@ -1,16 +1,49 @@
 import numpy as np
 
-
 class AdvancedSampler(object):
 
     def __init__(self, env):
         self.env = env
+        self.keys = ['state', 'action', 'reward', 'done']
 
-    def rollout(self):
-        pass
+    def create_path(self):
+        self.paths = {}
+        for key in self.keys:
+            self.paths[key] = []
+        self.n_path = 0
+        self.n_transitions = 0
 
-    def rollous(self):
-        pass
+    def rollout(self, policy):
+        state = self.env.reset()
+        done = False
+        while not done:
+            self.n_transitions += 1
+            action = policy(state)
+            next_state, reward, done, _ = self.env.step(action)
+            yield state, action, reward, done
+            state = next_state
 
-    def get_adv(self):
-        pass
+    def rollous(self, policy, n_trans):
+        self.create_path()
+        while len(self.paths) < n_trans:
+            for transition in self.rollout(policy):
+                for key, tran in zip(self.keys, transition):
+                    self.paths[key].append(tran)
+            self.n_path += 1
+        for key in self.keys:
+            self.paths[key] = np.asarray(self.paths[key])
+            if self.paths[key].ndim == 1:
+                self.paths[key] = np.expand_dims(self.paths[key], axis=-1)
+        self.paths['n_path'] = self.n_path
+
+    def get_adv(self, policy, val_fn, discount, lam):
+        values = val_fn.predict(self.paths['states'])
+        advantages = np.empty_like(self.paths['reward'])
+        for k, val in enumerate(reversed(self.paths['reward'])):
+            t = len(self.paths['reward']) - k - 1
+            if self.paths['done']: #
+                advantages[t] = self.paths['reward'][k] - values[k]
+            else:
+                sigma = self.paths['reward'][k] + discount * values[k+1] - values[k]
+                advantages[t] = sigma + lam * advantages[k+1]
+        return advantages, values
